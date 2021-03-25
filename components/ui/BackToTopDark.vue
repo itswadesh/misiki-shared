eac7d4ed6bfa4ee188e4336e9dd7bb401203b16b
Swadesh Behera <itswadesh@gmail.com>
2021-03-25 23:44:48 +0530

    backtotop

--- components/ui/BackToTopDark.vue
+++ components/ui/BackToTopDark.vue
@@ -1,10 +1,23 @@
 <template>
   <button
-    class="fixed bottom-0 right-0 z-10 block w-8 h-8 text-xl text-white bg-gray-500 border-0 rounded-full outline-none cursor-pointer hover:bg-gray-600"
+    class="fixed bottom-0 right-0 z-10 flex items-center w-8 h-8 mb-3 mr-3 text-xl text-center text-gray-900 bg-gray-300 border border-gray-600 rounded-full shadow-lg outline-none cursor-pointer focus:outline-none hover:bg-gray-600 hover:text-white"
     @click="toTop"
     v-if="scrolled > 1000"
   >
-    <img class="w-1/3 m-auto" v-lazy="`/up-arrow.png`" alt />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      class="w-4 m-2"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M5 10l7-7m0 0l7 7m-7-7v18"
+      />
+    </svg>
   </button>
 </template>
 
