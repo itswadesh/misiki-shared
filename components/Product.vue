e1d32bf1d8fd051f522d2ca8ecfe4e70223ca036
Swadesh Behera <itswadesh@gmail.com>
2021-03-27 19:11:55 +0530

    Product component p->product

--- components/Product.vue
+++ components/Product.vue
@@ -4,24 +4,24 @@
     v-if="p"
   >
     <!-- <img
-      v-lazy="p.type === 'V' ? '/veg.png' : '/non-veg.png'"
+      v-lazy="product.type === 'V' ? '/veg.png' : '/non-veg.png'"
       class="absolute top-0 left-0 z-50 w-5 mt-1 ml-1 bg-white"
     /> -->
-    <nuxt-link :to="`/${p.slug}`" class="relative block">
+    <nuxt-link :to="`/${product.slug}`" class="relative block">
       <div
-        v-if="p.time == '12 - 2 PM'"
+        v-if="product.time == '12 - 2 PM'"
         class="absolute right-0 z-10 px-4 text-xs text-white bg-red-500 rounded-l-lg opacity-75"
       >
         Lunch
       </div>
       <div
-        v-else-if="p.time == '8:30 - 9:30 PM'"
+        v-else-if="product.time == '8:30 - 9:30 PM'"
         class="absolute right-0 z-10 px-4 text-xs text-white bg-blue-500 rounded-l-lg opacity-75"
       >
         Dinner
       </div>
       <img
-        v-lazy="`${p.img}`"
+        v-lazy="`${product.img}`"
         alt
         class="object-cover w-full h-32 mb-2 rounded"
       />
@@ -29,38 +29,38 @@
     <div class="px-2">
       <div class="relative">
         <div class="h-6">
-          <div v-if="p.ratings" class="flex items-center">
-            <Ratingcircle :rating="p.ratings" class="m-review" />
-            <div class="pl-2 text-xs">{{ p.reviews }}</div>
+          <div v-if="product.ratings" class="flex items-center">
+            <Ratingcircle :rating="product.ratings" class="m-review" />
+            <div class="pl-2 text-xs">{{ product.reviews }}</div>
           </div>
         </div>
-        <nuxt-link :to="`/${p.slug}`">
+        <nuxt-link :to="`/${product.slug}`">
           <h2 class="h-8 pr-2 text-xs font-bold tracking-wide line-clamp-2">
-            {{ p.name }}
+            {{ product.name }}
           </h2>
         </nuxt-link>
-        <p>{{ p.description }}</p>
+        <p>{{ product.description }}</p>
         <div class="flex flex-wrap items-center justify-between my-2">
           <div class="font-bold">
-            {{ p.price | currency(settings.currency_symbol) }}
+            {{ product.price | currency(settings.currency_symbol) }}
           </div>
           <del class="text-xs text-gray-700">
-            {{ p.mrp | currency(settings.currency_symbol) }}
+            {{ product.mrp | currency(settings.currency_symbol) }}
           </del>
-          <div v-if="p.stock < 1" class="text-xs text-red-500">
+          <div v-if="product.stock < 1" class="text-xs text-red-500">
             Out of stock
           </div>
           <div v-else class="text-xs text-red-500">
-            <span v-if="p.stock < 6">Only</span> {{ p.stock }} left
+            <span v-if="product.stock < 6">Only</span> {{ product.stock }} left
           </div>
           <img
-            v-lazy="p.type === 'V' ? '/veg.png' : '/non-veg.png'"
+            v-lazy="product.type === 'V' ? '/veg.png' : '/non-veg.png'"
             class="w-5 mt-1 mr-1"
           />
         </div>
       </div>
-      <div class="bottom-0 py-1 text-xs border-t" v-if="p.vendor">
-        By {{ p.vendor.info.restaurant }}
+      <div class="bottom-0 py-1 text-xs border-t" v-if="product.vendor">
+        By {{ product.vendor.info.restaurant }}
       </div>
     </div>
   </div>
@@ -68,11 +68,11 @@
 
 <script>
 import { mapGetters } from 'vuex'
-import { Ratingcircle } from './ui'
+// import { Ratingcircle } from './ui'
 export default {
-  props: ['p'],
+  props: ['product'],
   methods: {},
-  components: { Ratingcircle },
+  // components: { Ratingcircle },
   computed: { ...mapGetters({ settings: 'settings' }) },
 }
 </script>
